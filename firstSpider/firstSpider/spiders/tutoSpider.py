1cdd85dd9914606beb137eb9231daf5cebbc5300
Luciano <lucianostrika44@gmail.com>
2017-04-16 23:13:05 -0300

    Update tutoSpider.py
    Removed unnecessary comments and tidied up a little

--- firstSpider/firstSpider/spiders/tutoSpider.py
+++ firstSpider/firstSpider/spiders/tutoSpider.py
@@ -1,18 +1,12 @@
 import scrapy
-import re
 class quotesSpider(scrapy.Spider):
 	name = 'earthporn'
 	start_urls = [
 		'http://reddit.com/r/earthporn'
 		]
-	count = 0
+	pageCount = 0 #pageCount
 	def parse(self,response):
-		'''page = response.url.split('/')[-2] #esto es por el formato de estos links
-		filename = 'quotes-%s.html' % page
-		with open(filename,'wb') as f: 
-			f.write('asdasd')
-		print ('done ' + filename)'''
-		final = '<script>var a = 5;</script>'
+		final = ''
 		for i in response.xpath('//a/@href'):
 			if i.extract()[-4:]=='.jpg' or i.extract()[-4:]=='.png':
 				final+='<img width=\'30%\' height=\'30%\' src=\''+ i.extract()+'\' > </img><br/> '
@@ -23,7 +17,7 @@ class quotesSpider(scrapy.Spider):
 		print('all is right')
 		nextLink = response.css('span.next-button a::attr(href)')[0].extract()
 		print(nextLink)
-		self.count += 1
-		if self.count<10:
+		self.pageCount += 1
+		if self.pageCount<10: #this number can be changed to an arbitrary amount of pages.
 			yield scrapy.Request(url = nextLink,callback = self.parse)
 		
